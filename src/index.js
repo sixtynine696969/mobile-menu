a4c7b4be95cafbef0ad440a15425f97c177654ba
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-08-31 04:13:04 +0200

    lint index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
 import './style.css';
 
 const menu = document.querySelector('.menu');
-const display = document.querySelector('.display')
+const display = document.querySelector('.display');
 const burgerMenuButton = document.querySelector('input.burger-menu');
 const mainContent = document.body.querySelector('.content');
 const menuButtons = document.querySelectorAll('.menu button');
 
 function hideMenu() {
-    menu.style.display = 'none';
-    display.style.gridTemplateColumns = '';
+  menu.style.display = 'none';
+  display.style.gridTemplateColumns = '';
 }
 
 function showMenu() {
-    menu.style.display = 'flex';
-    display.style.gridTemplateColumns = 'calc(var(--phone-width) / 2.5) calc(var(--phone-width) - 10px)';
+  menu.style.display = 'flex';
+  display.style.gridTemplateColumns = 'calc(var(--phone-width) / 2.5) calc(var(--phone-width) - 10px)';
 }
 
 burgerMenuButton.addEventListener('click', (e) => {
-    const menuStyles = getComputedStyle(menu);
-    if (menuStyles.display === 'none') {
-        showMenu();
-    } else {
-        hideMenu();
-    }
-})
+  const menuStyles = getComputedStyle(menu);
+  if (menuStyles.display === 'none') {
+    showMenu();
+  } else {
+    hideMenu();
+  }
+});
 
 mainContent.addEventListener('click', (e) => {
-    if (e.target.classList.contains('burger-menu')) return;
+  if (e.target.classList.contains('burger-menu')) return;
+  hideMenu();
+});
+
+menuButtons.forEach((btn) => {
+  btn.addEventListener('click', () => {
     hideMenu();
-})
-
-
-menuButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        hideMenu();
-    })
-})
+  });
+});
